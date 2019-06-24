543cfbef670c3865a7ed21f9afca91a8eb4dcbfd
Scott Brown <scurry99@hotmail.co.uk>
2019-06-24 18:22:33 +0100

    3d start

--- OpenGL Test/OpenGL Test/Source.cpp
+++ OpenGL Test/OpenGL Test/Source.cpp
@@ -14,7 +14,7 @@
 
 float mixer = 0.5f;
 float xScale = 1.0f;
-glm::mat4 trans = glm::mat4(1.0f);
+
 
 
 int main()
@@ -52,17 +52,47 @@ int main()
 	glfwSetKeyCallback(window, key_callback);
 
 	float vertices[] = {
-	 1.0f, 1.0f, 0.0f,  // top right 0
-	 1.0f, -1.0f, 0.0f,  // bottom right 1
-	 -1.0f, -1.0f, 0.0f,  // bottom left 2
-	 -1.0f, 1.0f, 0.0f,	// top left 3
-	 0.0f, 0.0f, 1.0f, // middle  4
-	 -1.0f, 0.0f, 0.5f, // middle left 5
-	 0.0f, 1.0f, 0.5f, // middle top 6
-	 1.0f, 0.0f, 0.5f, // middle right 7
-	 0.0f, -1.0f, 0.5f // middle bottom 8
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
 
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
 	float tringley[] = {
@@ -198,8 +228,6 @@ int main()
 	glEnableVertexAttribArray(0);
 
 	
-	glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f);
-	glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
 
 	void processInput(GLFWwindow *window);
 	
@@ -234,11 +262,40 @@ int main()
 		glUniform1i(glGetUniformLocation(ourShader.ID, "texture2"), 1);
 		glUniform1f(glGetUniformLocation(ourShader.ID, "mixer"), mixer);
 		
-		/*glm::mat4 trans = glm::mat4(1.0f);
-		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
-		trans = glm::translate(trans, glm::vec3(-0.5f, -0.5f, 0.0f));
-		trans = glm::scale(trans, glm::vec3(xScale, 1.0, 1.0));*/
+
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+
+		glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f);
+
+		glm::mat4 projection = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+
+		projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 		
+
+		int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+		int viewLoc = glGetUniformLocation(ourShader.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+		int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
+		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+		
+		// note that we're translating the scene in the reverse direction of where we want to move
+		
+
+		glm::mat4 trans = glm::mat4(1.0f);
+		bool shouldTrans = false;
+		if (shouldTrans) {
+			trans = glm::translate(trans, glm::vec3(0.5f, 0.5f, 0.0f));
+			trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
+			trans = glm::scale(trans, glm::vec3(xScale, 1.0, 1.0));
+		}
 		unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
 		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
--- OpenGL Test/OpenGL Test/vShader.vs
+++ OpenGL Test/OpenGL Test/vShader.vs
@@ -4,10 +4,14 @@ layout (location = 1) in vec3 aColor;
 layout (location = 2) in vec2 aTexCoord;
 out vec3 uColor;
 out vec2 TexCoord;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 uniform mat4 transform;
 void main()
 {
-gl_Position = transform * vec4(aPos, 1.0f);
+gl_Position = projection * view * model * transform * vec4(aPos, 1.0f);
 uColor = aColor;
 TexCoord = aTexCoord;
 }
